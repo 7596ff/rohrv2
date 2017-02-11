a39924bd9311cf6b36ee77a4b08c42ce568584b7
Cassandra McCarthy <cassie@7596ff.com>
2017-02-11 01:18:05 -0500

    pretty help

--- commands/help.js
+++ commands/help.js
@@ -1,14 +1,26 @@
+const util = require("util");
+
 module.exports = (message) => {
+    let me = message.channel.guild.members.has("102645408223731712") ? "<@102645408223731712>" : "alexa#2899";
+
     let help = [
         "help for rohrkatze rotator",
         "",
         "`katze ping` - pong",
-        "`katze pls` - add an image attachment or icon to rotate (requires the manage server permission)",
+        "`katze pls` - add an image attachment or icon to rotate ",
+        "(requires the manage server permission)",
         "`katze rotate` - rotates the server icon",
         "`katze list` - lists all current server icons",
         "`katze delete <img>` - after `katze list`ing, delete a specific icon",
+        "`katze invite` - invite link",
         "",
-        "the server will rotate the icon every 24 hours at midnght EST. contact alexa#2899 to change this timeout"
+        `the server will rotate the icon every 24 hours at midnght EST. contact ${me} to change this timeout`
     ];
-    message.channel.createMessage(help.join("\n"));
+    
+    message.channel.createMessage({"embed": {
+        "timestamp": new Date().toJSON(),
+        "description": help.join("\n")
+    }}).then(() => {
+        util.log(`sent info message to ${message.channel.guild.name}`);
+    }).catch(err => util.error(err));
 };
