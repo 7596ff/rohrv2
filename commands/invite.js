63b5a92623bc7410d2675cac8b92abcb04a0b866
brendan10211 <brendan10211@gmail.com>
2017-04-07 09:55:24 -0400

    weebscale part 2

--- commands/invite.js
+++ commands/invite.js
@@ -1,3 +1,3 @@
 module.exports = (message) => {
-    message.channel.createMessage("https://discordapp.com/oauth2/authorize?permissions=32&scope=bot&client_id=254387001556598785");
+    message.channel.createMessage("https://discordapp.com/oauth2/authorize?permissions=268435488&scope=bot&client_id=254387001556598785");
 };
