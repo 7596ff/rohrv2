a68ef68e562dec4430bf64ba636a88a7075b6491
brendan10211 <brendan10211@gmail.com>
2017-04-17 14:40:07 -0400

    what

--- commands/do.js
+++ commands/do.js
@@ -7,13 +7,10 @@ module.exports = (message) => {
         return;
     }
 
-    let task = message._client.tasks[message.channel.guild.id];
-    if (task) {
-        resched(message._client, message.channel.guild.id);
-        message._client.gcfg[message.channel.guild.id].dont = false;
-        fs.writeFile("./gcfg.json", JSON.stringify(message._client.gcfg), (err) => {
-            if (err) util.error(err);
-            message.channel.createMessage(":white_check_mark: ok :)");
-        });
-    }
+    resched(message._client, message.channel.guild.id);
+    message._client.gcfg[message.channel.guild.id].dont = false;
+    fs.writeFile("./gcfg.json", JSON.stringify(message._client.gcfg), (err) => {
+        if (err) util.error(err);
+        message.channel.createMessage(":white_check_mark: ok :)");
+    });
 };
