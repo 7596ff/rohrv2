9b94380a1829d2b9857706324d4b220246ef4d0e
Cassandra McCarthy <cassie@7596ff.com>
2017-04-19 16:25:53 -0400

    starboard as a database

--- util/pg.js
+++ util/pg.js
@@ -6,7 +6,7 @@ class Pg {
     addGuild(guildID) {
         if (!guildID) return new Error("no guild ID");
         return this.postgres.query({
-            "text": "INSERT INTO guilds (id, current, lasttime, timeout, meme, activityrole, dont, starboard, emoji) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9);",
+            "text": "INSERT INTO guilds VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9);",
             "values": [guildID, 0, 0, 12, false, 0, false, 0, "⭐"]
         });
     }
@@ -20,7 +20,7 @@ class Pg {
     }
 
     getGcfg(guildID) {
-        if (!guildID) return new Error("no guild ID");
+
         return new Promise((resolve, reject) => {
             this.postgres.query({
                 "text": "SELECT * FROM guilds WHERE id = $1;",
@@ -32,7 +32,6 @@ class Pg {
     }
 
     updateCurrent(guildID, imageID) {
-        if (!guildID) return new Error("no gulid ID");
         return this.postgres.query({
             "text": "UPDATE guilds SET current = $1, lasttime = $2 WHERE id = $3;",
             "values": [imageID, Date.now(), guildID]
@@ -40,7 +39,6 @@ class Pg {
     }
 
     updateTimeout(guildID, timeout) {
-        if (!guildID) return new Error("no gulid ID");
         return this.postgres.query({
             "text": "UPDATE guilds SET timeout = $1 WHERE id = $2;",
             "values": [timeout, guildID]
@@ -48,7 +46,6 @@ class Pg {
     }
 
     flipMeme(guildID) {
-        if (!guildID) return new Error("no gulid ID");
         return new Promise((resolve, reject) => {
             this.postgres.query({
                 "text": "SELECT (meme) FROM guilds WHERE id = $1;",
@@ -68,7 +65,6 @@ class Pg {
     }
 
     updateActivity(guildID, roleID) {
-        if (!guildID) return new Error("no gulid ID");
         return this.postgres.query({
             "text": "UPDATE guilds SET activityrole = $1 WHERE id = $2;",
             "values": [roleID || 0, guildID]
@@ -76,7 +72,6 @@ class Pg {
     }
 
     makeDont(guildID) {
-        if (!guildID) return new Error("no gulid ID");
         return this.postgres.query({
             "text": "UPDATE guilds SET dont = 't' WHERE id = $1;",
             "values": [guildID]
@@ -84,7 +79,6 @@ class Pg {
     }
 
     makeDo(guildID) {
-        if (!guildID) return new Error("no gulid ID");
         return this.postgres.query({
             "text": "UPDATE guilds SET dont = 'f' WHERE id = $1;",
             "values": [guildID]
@@ -92,7 +86,6 @@ class Pg {
     }
 
     updateStarboardChannel(guildID, channelID) {
-        if (!guildID) return new Error("no gulid ID");
         return this.postgres.query({
             "text": "UPDATE guilds SET starboard = $1 WHERE id = $2;",
             "values": [channelID || 0, guildID]
@@ -100,10 +93,98 @@ class Pg {
     }
 
     updateStarboardEmoji(guildID, emoji) {
-        if (!guildID) return new Error("no gulid ID");
         return this.postgres.query({
             "text": "UPDATE guilds SET emoji = $1 WHERE id = $2;",
             "values": [emoji || "⭐", guildID]
+        });
+    }
+
+    getStarCfg(guildID) {
+        return new Promise((resolve, reject) => {
+            this.postgres.query({
+                "text": "SELECT starboard, emoji FROM guilds WHERE id = $1;",
+                "values": [guildID]
+            }).catch((err) => reject(err)).then((res) => {
+                resolve(res.rows[0]);
+            });
+        });
+    }
+
+    getStarStatus(messageID) {
+        return new Promise((resolve, reject) => {
+            this.postgres.query({
+                "text": "SELECT * FROM starboard WHERE msg = $1;",
+                "values": [messageID]
+            }).catch((err) => reject(err)).then((res) => {
+                if (res.rows.length == 1) {
+                    resolve(res.rows[0])
+                } else {
+                    resolve("dne");
+                }
+            });
+        });
+    }
+
+    createStar(guildID, memberID, channelID, msgID, memberName, channelName, firstID) {
+        return new Promise((resolve, reject) => {
+            this.postgres.query({
+                "text": "INSERT INTO starboard VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10);",
+                "values": [
+                    Date.now(),
+                    guildID,
+                    memberID,
+                    channelID,
+                    msgID,
+                    0,
+                    memberName,
+                    channelName,
+                    1,
+                    { "who": [firstID] }
+                ]
+            }).catch((err) => reject(err)).then((res) => resolve(res.rows[0]));
+        });
+    }
+
+    incrementStar(msgID, whoID) {
+        return new Promise((resolve, reject) => {
+            this.postgres.query({
+                "text": "SELECT who FROM starboard WHERE msg = $1;",
+                "values": [msgID]
+            }).catch((err) => reject(err)).then((res) => {
+                let who = res.rows[0].who.who;
+                who.push(whoID);
+                who = who.filter((item, index, array) => index === array.indexOf(item));
+
+                this.postgres.query({
+                    "text": "UPDATE starboard SET stars = (stars + 1), who = $1 WHERE msg = $2;",
+                    "values": [{ "who": who }, msgID]
+                }).catch((err) => reject(err)).then((res) => resolve(res.rows[0]));
+            });
+        });
+    }
+
+    decrementStar(msgID, whoID) {
+        return new Promise((resolve, reject) => {
+            this.postgres.query({
+                "text": "SELECT who FROM starboard WHERE msg = $1;",
+                "values": [msgID]
+            }).catch((err) => reject(err)).then((res) => {
+                let who = res.rows[0].who.who;
+                who.splice(who.indexOf(whoID), 1);
+                who = who.filter((item, index, array) => index === array.indexOf(item));
+
+                if (who.length > 0) {
+                    this.postgres.query({
+                        "text": "UPDATE starboard SET stars = (stars - 1), who = $1 WHERE msg = $2;",
+                        "values": [{ "who": who }, msgID]
+                    }).catch((err) => reject(err)).then((res) => resolve(res.rows[0]));
+                } else {
+                    this.postgres.query({
+                        "text": "DELETE FROM starboard WHERE msg = $1;",
+                        "values": [msgID]
+                    }).catch((err) => reject(err)).then((res) => resolve("dne"));
+                }
+            });
         });
     }
 }
