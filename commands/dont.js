e7d753933efb742dfeda212674d5551f8986ae7e
Cassandra McCarthy <cassie@7596ff.com>
2017-04-20 13:37:55 -0400

    fix dont on guilds with no images

--- commands/dont.js
+++ commands/dont.js
@@ -5,17 +5,16 @@ async function dont(message, client) {
     }
 
     let task = message._client.tasks[message.channel.guild.id];
-    if (task) {
-        try {
-            await client.pg.makeDont(message.channel.guild.id);
-            task.cancel();
-            delete client.gcfg[message.channel.guild.id];
-            message.channel.createMessage(":white_check_mark: ok :(");
-        } catch (err) {
-            console.error(err);
-            message.channel.createMessage(":x: something went wrong donting, try again maybe");
-        }
+    try {
+        await client.pg.makeDont(message.channel.guild.id);
+        if (task) task.cancel();
+        delete client.gcfg[message.channel.guild.id];
+        message.channel.createMessage(":white_check_mark: ok :(");
+    } catch (err) {
+        console.error(err);
+        message.channel.createMessage(":x: something went wrong donting, try again maybe");
     }
+
 }
 
 module.exports = dont;
