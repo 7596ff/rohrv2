796c3978b7c078b828141e0f80b41277cb203c1e
brendan10211 <brendan10211@gmail.com>
2017-06-08 04:08:38 -0400

    AAAAA

--- commands/farm.js
+++ commands/farm.js
@@ -3,7 +3,7 @@ function farm(message) {
     let discrim = (!isNaN(num) && num) || (message.mentions.length && message.mentions[0].discriminator) || message.author.discriminator;
     if (!discrim) return message.channel.createMessage("uh no discrim what???");
 
-    let map = message._client.users.filter((user) => user.discriminator == discrim).map((user) => `${user.username}#${user.discriminator}`);
+    let map = message._client.users.filter((user) => user.discriminator == discrim).map((user) => `${user.username}#${user.discriminator}`).join(", ");
     return message.channel.createMessage("```js\n" + map + "\n```");
 }
 
